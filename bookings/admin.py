366462bc5cd632b462c08ea5543f8839e59abefa
Robyn Smith <robyn.roybin@gmail.com>
2023-01-30 21:53:35 +0000

    Create base template

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'party_size', 'date_time',
+                    'special_occasion', 'special_requirements', 'confirmed')
+    search_fields = ('name', 'confirm', 'email')
+    list_filter = ('name', 'party_size')
+    actions = ['approve_bookings']
+
+    def approve_bookings(self, request, queryset):
+        queryset.update(confirmed=True)
